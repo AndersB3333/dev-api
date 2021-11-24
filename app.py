a21031ec1dbcc08f9ccb37fae89edc6f4f52eb2a
anders baumann <andersbaumann@gmail.com>
2021-11-23 18:42:42 -0800

    Adding the required files to setup an API

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request, jsonify
+from flask_restful import Resource, Api
+import numpy as np
+import pandas as pd
+import random as random
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hand(Resource):
+    def post(self):
+        request_data = request.get_json()
+        return request_data
+
+api.add_resource(Hand, '/')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
